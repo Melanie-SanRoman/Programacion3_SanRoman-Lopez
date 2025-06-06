ac6171cbc54f334c911d0b6486f66479235b1b18
Melanie San Roman <srmel123@gmail.com>
2025-06-06 19:37:16 -0300

    Se agregaron los esqueletos de las clases Backtrackin y Greedy

---
+++ Backtracking.java
@@ -0,0 +1,15 @@
+package tp_especial;
+
+public class Backtracking {
+    
+    /*
+     * <<Breve explicación de la estrategia de resolución. Por ejemplo:
+     * - Cómo se genera el árbol de exploración.
+     * - Cuáles son los estados finales y estados solución.
+     * - Posibles podas.
+     * - etc.>>
+     */
+    public Solucion backtracking() {
+        return null;
+    }
+}
---
+++ Greedy.java
@@ -0,0 +1,15 @@
+package tp_especial;
+
+public class Greedy{
+     
+       /*
+     * <<Breve explicación de la estrategia de resolución. Por ejemplo:
+     * - Cuáles son los candidatos.
+     * - Estrategia de selección de candidatos.
+     * - Consideraciones respecto a encontrar o no solución.
+     * - etc.>>
+     */
+    public Solucion greedy() {
+        return null;
+    }
+}
