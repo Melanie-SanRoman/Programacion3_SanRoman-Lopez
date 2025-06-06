9f8ce0c144ae24b4e6e7d84c04c3981d9420bf67
Melanie San Roman <srmel123@gmail.com>
2025-06-06 19:33:47 -0300

    Se agrega clase Fabrica que contiene una lista de futuras maquinas

---
+++ Fabrica.java
@@ -0,0 +1,13 @@
+package tp_especial;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fabrica{
+
+    List<Maquina> maquinasDisponibles;
+
+    public Fabrica(){
+        this.maquinasDisponibles= new ArrayList<>();
+    }
+}
