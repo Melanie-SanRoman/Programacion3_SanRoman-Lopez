ba5b3c7528a1a4be5306c7f60b168fcfbcafa14e
Melanie San Roman <srmel123@gmail.com>
2025-06-12 21:37:49 -0300

    Se implementaron los metodos necesarios para controlar los estados

--- Estado.java
+++ Estado.java
@@ -5,12 +5,37 @@ import java.util.List;
 
 public class Estado {
     List<Maquina> maquinasUtilizadas;
+    List<Maquina> maquinasDisponibles;
     int piezasCreadas;
     int piezasTotales;
 
-    public Estado(int piezasTotales) {
+    public Estado(List<Maquina> maquinasDisponibles, int piezasTotales) {
         this.maquinasUtilizadas = new ArrayList<>();
         this.piezasCreadas = 0;
         this.piezasTotales = piezasTotales;
+        this.maquinasDisponibles = maquinasDisponibles;
     }
+
+    public List<Maquina> getMaquinasDisponibles() {
+        return maquinasDisponibles;
+    }
+
+    public int getPiezasTotales() {
+        return piezasTotales;
+    }
+
+    public int getPiezasCreadas() {
+        return piezasCreadas;
+    }
+
+    public void agregar(Maquina maquina) {
+        maquinasUtilizadas.add(maquina);
+        piezasCreadas += maquina.getPiezas();
+    }
+
+    public void quitar(Maquina maquina) {
+        maquinasUtilizadas.remove(maquinasUtilizadas.size() - 1); // elimina la ultima maquina
+        piezasCreadas -= maquina.getPiezas();
+    }
+
 }
